fe75f16cc96a5fd4f0ceb34f6b63cbf9e7fb1892
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-09 17:28:01 -0300

    Add contacts into schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_09_191419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.date "birth_date"
+    t.string "phone"
+    t.string "address"
+    t.string "credit_card"
+    t.string "franchise"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
