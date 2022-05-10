b093aecd8e320d5ac7d012d3ac3777bbe7e485a0
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-10 09:27:12 -0300

    Require credit card validations gem

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'credit_card_validations/string'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
