80bb881a0aff5593c92f6750c2ef741a34994ae1
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-10 13:48:59 -0300

    Set resources csv_import/import collection on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'contacts#index'
   resources :contacts, only: [:index, :create, :new]
+  resources :csv_import do
+    collection { post :import }
+  end
 end
+
