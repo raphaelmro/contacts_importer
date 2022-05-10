d54f3d8aca9a871d659275e566707e5a84af6a6a
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-10 09:33:04 -0300

    Start developing user_spec model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { FactoryBot.build(:user) }
+
+  describe 'associations' do
+    context 'have_many' do
+      it { is_expected.to have_many(:contacts).dependent(:destroy) }
+    end
+  end
+
 end
