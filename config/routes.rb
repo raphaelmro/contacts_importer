c531d662254c33a63411229f2f256b465fced1bf
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-11 01:59:57 -0300

    Add route to watch the sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   devise_for :users
   root 'contacts#index'
   resources :contacts, only: [:index, :create, :new]
