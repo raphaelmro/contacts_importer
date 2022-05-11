7c2e7034137e4ca3322aacb7385a759ccf604aa3
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-11 02:00:19 -0300

    Update status while processing a csv file import

--- app/jobs/import_csv_file_job.rb
+++ app/jobs/import_csv_file_job.rb
@@ -4,9 +4,15 @@ class ImportCsvFileJob < ApplicationJob
   def perform(file_path_to_save_to, original_filename, current_user)
     CsvImport.create!(user_id: current_user.id, filename: original_filename)
     CSV.foreach(file_path_to_save_to, headers: true) do |row|
+      CsvImport.update(status: 'processing')
       csv_hash = row.to_hash
       csv_hash['user_id'] = current_user.id
-      Contact.create!(csv_hash)
+      if Contact.create!(csv_hash)
+        CsvImport.update(status: 'finished')
+      else
+        CsvImport.update(status: 'failed')
+      end
     end
+    File.delete(file_path_to_save_to)
   end
 end
