6b2d0e7744092955c3de7c99417c26fcb4b3aebb
Raphael Onofre <raphael.onofre@gmail.com>
2022-05-11 01:58:13 -0300

    Create update and adjust import action on csv_import_controller.rb

--- app/controllers/csv_import_controller.rb
+++ app/controllers/csv_import_controller.rb
@@ -1,8 +1,9 @@
 class CsvImportController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_csv_import, only: [:update]
 
   def index
-    # @csv_imports = CsvImport.where(user_id: current_user.id)
+    @csv_imports = CsvImport.where(user_id: current_user.id)
   end
 
   def new
@@ -13,18 +14,28 @@ class CsvImportController < ApplicationController
     @csv_import = current_user.user.build(csv_import_params)
   end
 
+  def update
+    @csv_import.update(csv_import_params)
+  end
+
   def import
-    file_path_to_save_to = './tmp/' + params[:file].original_filename
-    File.write(file_path_to_save_to, params[:file].read)
-
-    ImportCsvFileJob.perform_later(file_path_to_save_to, params[:file].original_filename, current_user)
-
-    redirect_to contacts_path, notice: "CSV imported successfully"
+    if params[:file].nil?
+      redirect_to new_csv_import_path, alert: "Please choose a file to import"
+    else
+      file_path_to_save_to = './tmp/' + params[:file].original_filename
+      File.write(file_path_to_save_to, params[:file].read)
+      ImportCsvFileJob.perform_later(file_path_to_save_to, params[:file].original_filename, current_user)
+      redirect_to contacts_path, notice: "CSV imported successfully"
+    end
   end
 
   private
+  def set_csv_import
+    @csv_import = CsvImport.find(params[:id])
+  end
+
   def csv_import_params
-    params.require(:csv_import).permit(:status, :user_id)
+    params.require(:csv_import).permit(:filename, :status, :user_id)
   end
 
 end
